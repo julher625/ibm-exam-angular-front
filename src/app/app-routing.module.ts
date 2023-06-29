dbe7b2e703d9b236b58abdc554d69ae851de240c
Julian Hernandez <julian.hernandez.bohorquez@ibm.com>
2023-06-28 23:25:10 -0500

    Add initial components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio/inicio.component';
+import { FormularioComponent } from './formulario/formulario/formulario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "inicio", component: InicioComponent},
+  { path: "formulario", component: FormularioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio/inicio.component';
+import { FormularioComponent } from './formulario/formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
