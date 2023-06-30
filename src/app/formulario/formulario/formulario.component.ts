4ff90a31702b9d9e88ae227a4316cf1077355994
Julian Hernandez <julian.hernandez.bohorquez@ibm.com>
2023-06-30 00:42:07 -0500

    Mask the document

--- src/app/formulario/formulario/formulario.component.ts
+++ src/app/formulario/formulario/formulario.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class FormularioComponent {
 
+  documentoEnmascarado?: string;
+  documento?: string;
+
+
+
+  encriptarNumeroDocumento() {
+    const numeroDocumento = (<HTMLInputElement>document.getElementById('documento')).value;
+    console.log(numeroDocumento.substring(-2));
+    this.documento = numeroDocumento;
+    this.documentoEnmascarado = '*****' + numeroDocumento.substring(numeroDocumento.length -2);
+    // const documentoCifrado = encriptarDocumento(numeroDocumento);
+    // (<HTMLInputElement>document.getElementById('documentoCifrado')).value = documentoCifrado;
+  }
+
+  update(element:string, value:string){
+    (<HTMLInputElement>document.getElementById(element)).value = value;
+  }
+
+  getInputValue(event: Event):void {
+    console.log(event);
+    // const target = event.target as HTMLInputElement;
+    
+
+
+    if((event.target as HTMLInputElement).id == "nombre"){
+      this.encriptarNumeroDocumento()
+      if(this.documentoEnmascarado){
+        this.update('documento',this.documentoEnmascarado)
+      }
+
+    }else if((event.target as HTMLInputElement).id == "documento"){
+      if(this.documento){
+        this.update('documento',this.documento)
+      }
+    }
+
+  }
+
 }
